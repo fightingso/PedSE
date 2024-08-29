77c50d19841bcb945f8692a6ffd4dc672edea133
fightingso <hirawataso@gmail.com>
2024-08-29 20:30:18 +0900

    chore: Simplify genetic algorithm code structure, unify classes, and optimize operations

---
+++ pedse/core/__init__.py
@@ -0,0 +1,9 @@
+from .ppop import Individual, Population
+from .wpop import WholeIndividual, WholePopulation
+
+__annotations__ = {
+    'Individual': Individual,
+    'Population': Population,
+    'WholeIndividual': WholeIndividual,
+    'WholePopulation': WholePopulation,
+}
